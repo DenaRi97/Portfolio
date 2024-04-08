a8c4c51b63868b43bc41c5ebf05946be5a5b1dbd
Almudena Rivera <riveraferalmudena@gmail.com>
2024-04-08 14:37:51 +0200

    netlify deploy

--- FRONT/tailwind.config.js
+++ FRONT/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui", 'tailwindcss-animated')],
   colors: {
   },
   backgroundImage: {
