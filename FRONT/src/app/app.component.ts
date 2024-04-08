b589034dfdf75c8c9d8865744366f71e7909a7a4
Almudena Rivera <riveraferalmudena@gmail.com>
2024-04-08 11:40:38 +0200

    trying temp deploy

--- FRONT/src/app/app.component.ts
+++ FRONT/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent]
+    imports: [RouterOutlet, NavbarComponent, AboutComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'Portfolio';
--- FRONT/src/app/app.routes.ts
+++ FRONT/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: LandingComponent, 
-        
+        component: LandingComponent 
     },
+    // {
+    //     path: 'works',
+    //     component: ,
+    // },
 ];
